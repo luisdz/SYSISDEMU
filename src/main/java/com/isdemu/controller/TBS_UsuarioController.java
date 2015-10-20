c0cbc35b721c98e9b53881f3ab67ebe241b8b0f2
Jose Eduardo <Jose Eduardo@edu>
2015-10-19 22:36:44 -0600

    new run

--- src/main/java/com/isdemu/controller/TBS_UsuarioController.java
+++ src/main/java/com/isdemu/controller/TBS_UsuarioController.java
@@ -29,8 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.imageio.ImageIO;
-import jbarcodebean.JBarcodeBean;
-import net.sourceforge.jbarcodebean.model.Interleaved25;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
