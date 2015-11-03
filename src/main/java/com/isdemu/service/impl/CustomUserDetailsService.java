3dedb2d08084159c6c0ca03aa112ecb147f0a45a
Jose Eduardo <Jose Eduardo@edu>
2015-11-02 21:21:43 -0600

    security DB

--- src/main/java/com/isdemu/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/isdemu/service/impl/CustomUserDetailsService.java
@@ -7,6 +7,7 @@ package com.isdemu.service.impl;
 
 import com.isdemu.model.TbsRol;
 import com.isdemu.model.TbsUsuario;
+import com.isdemu.service.TBS_Rol_Service;
 import com.isdemu.service.TBS_Usuario_Service;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -24,30 +26,47 @@ import org.springframework.stereotype.Service;
  */
 @Service("customUserDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired private TBS_Usuario_Service userService;
     
-    public UserDetails loadUserByUsername(String ssoId)
-			throws UsernameNotFoundException {
-		TbsUsuario user = (TbsUsuario) userService.findByKey(1);
+    @Autowired 
+    private TBS_Usuario_Service userService;
+    
+    @Autowired 
+    private TBS_Rol_Service rolService;
+    
+    @Transactional(readOnly=true)
+    @SuppressWarnings("empty-statement")
+    public UserDetails loadUserByUsername(String username) 
+		
+		throws UsernameNotFoundException {
+		TbsUsuario user = new TbsUsuario();
+                  
+                
+                try {
+                    
+                    user = (TbsUsuario)(userService.findByNick(username).get(0));
+                }
+                catch(Exception e) 
+                {
+                   System.out.println("Usuario no encontrado");
+                };
+                  //TbsUsuario user = new TbsUsuario();
 		System.out.println("User : "+user);
-		if(user==null){
+		if(username==null){
 			System.out.println("User not found");
 			throw new UsernameNotFoundException("Username not found");
 		}
-			return new org.springframework.security.core.userdetails.User("user1","123456", 
-				 true, true, true, true,getGrantedAuthorities(user));
+			return new org.springframework.security.core.userdetails.User(user.getNombreUsuario(), user.getClave(), 
+				 true, true, true, true, getGrantedAuthorities(user));
 	}
     
     private List<GrantedAuthority> getGrantedAuthorities(TbsUsuario user){
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 		
-		
-			
-			authorities.add(new SimpleGrantedAuthority("ROLE_"));
+			authorities.add(new SimpleGrantedAuthority("ADMIN"));
 		
 		System.out.print("authorities :"+authorities);
 		return authorities;
-	}
+    }
     
     
 }
